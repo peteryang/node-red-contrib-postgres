f56e4562620d8d38d2e0a0f0114aed7459590a31
Hiroyuki Okada <okada@unirobot.com>
2019-07-11 10:48:44 +0900

    add grant

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,26 +5,16 @@ module.exports = function(grunt){
                 jshintrc:true     // Use external jshinrc file configured as below
             },
             all: {
-                src: ['*.js'],
-                filter: function(filepath) { // on some developer machines the test coverage HTML report utilities cause further failures
-                    if ((filepath.indexOf("coverage/") !== -1) || (filepath.indexOf("node_modules") !== -1)) {
-                        console.log( "\033[30m  filtered out \033[32m:\033[37m " + filepath + "\033[0m");
-                        return false;
-                    } else {
-                        return true;
-                    }
-                }
+                src: ['*.js']
             }
         },
         jsonlint:{
             all:{
-                src:[
-                    '/locales/**/*.json'
-                  ]
+                src: ['/locales/**/*.json']
             }
         },
         inlinelint: {
-            html: ['*.html', '!node_modules/*/*.html', '!*/node_modules/*.html'],
+            html: ['*.html'],
             options: {
                 jshintrc: ".jshintrc"
             }
